33bafcb322071aa1ef1e986d74917016c800a0e9
Gerson Toribio <gerson.ricardo.toribio.ossio@gmail.com>
2021-08-08 16:29:12 -0500

    Adding search with calendar  and loader bar

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,63 @@
+import { useState } from "react";
 import Image from "next/image";
 import {
   SearchIcon,
   GlobeAltIcon,
   MenuIcon,
+  UserIcon,
   UserCircleIcon,
 } from "@heroicons/react/solid";
+import { DateRangePicker } from "react-date-range";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { useRouter } from "next/dist/client/router";
 
-function Header() {
+function Header({ placeholder }) {
+  const [searchInput, setSeacrhInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [noOfGuests, setNoOgGuests] = useState(1);
+  const router = useRouter();
+
+  const search = () => {
+    router.push({
+      pathname: "/search",
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        noOfGuests,
+      },
+    });
+  };
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+    setSelectionRange({
+      startDate: ranges.selection.startDate,
+      endDate: ranges.selection.endDate,
+      key: "selection",
+    });
+  };
+
+  const resetInput = () => {
+    setSeacrhInput("");
+  };
+
+  const [selectionRange, setSelectionRange] = useState({
+    startDate,
+    endDate,
+    key: "selection",
+  });
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/** LEFT SECTION */}
-      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+      <div
+        onClick={() => router.push("/")}
+        className="relative flex items-center h-10 cursor-pointer my-auto"
+      >
         <Image
           src="https://links.papareact.com/qd3"
           layout="fill"
@@ -21,9 +68,11 @@ function Header() {
       {/** MIDDLE SECTION */}
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
         <input
+          value={searchInput}
+          onChange={(e) => setSeacrhInput(e.target.value)}
           className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
           type="text"
-          placeholder="Start your search"
+          placeholder={placeholder || "Start your search"}
         />
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
@@ -36,6 +85,38 @@ function Header() {
           <UserCircleIcon className="h-6 cursor-pointer" />
         </div>
       </div>
+      {/**DATE PICKER */}
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto mt-0">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
+          />
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of Guests
+            </h2>
+            <UserIcon className="h-5" />
+            <input
+              className="w-12 pl-2 outline-none text-lg text-red-400"
+              type="number"
+              value={noOfGuests}
+              min={1}
+              onChange={(e) => setNoOgGuests(e.target.value)}
+            />
+          </div>
+          <div className="flex items-center">
+            <button className="flex-grow text-gray-500" onClick={resetInput}>
+              Cancel
+            </button>
+            <button className="flex-grow text-red-400" onClick={search}>
+              Search
+            </button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
